f0db13a873840bad8b609884784eb54ef3aea05d
larsk21 <57503246+larsk21@users.noreply.github.com>
2021-10-20 10:58:16 +0200

    add result code and error handling
    - add Result enum
    - exit with result code
    - print error messages
    - add command line help

--- src/main/java/edu/kit/compiler/JavaEasyCompiler.java
+++ src/main/java/edu/kit/compiler/JavaEasyCompiler.java
@@ -11,34 +11,72 @@ import java.io.InputStreamReader;
 @Slf4j
 public class JavaEasyCompiler {
 
-    public static void main(CommandLine cmd) {
-        if (cmd.hasOption("e")) {
+    public static Result main(CommandLine cmd, Options options) {
+        if (cmd.hasOption("h")) {
+            HelpFormatter help = new HelpFormatter();
+            help.printHelp("Java Easy Compiler", options);
+
+            return Result.Ok;
+        }
+        else if (cmd.hasOption("e")) {
             String filePath = cmd.getOptionValue("e");
 
-            echo(filePath);
+            return echo(filePath);
         } else {
-            // error
+            System.err.println("Wrong command line arguments, see --help for supported commands.");
+
+            return Result.CliInputError;
         }
     }
 
-    private static void echo(String filePath) {
+    private static Result echo(String filePath) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
             reader.lines().forEachOrdered(line -> {
                 System.out.println(line);
             });
+
+            return Result.Ok;
         } catch (IOException e) {
-            // error
+            System.err.println("Error during file io: " + e.getMessage());
+
+            return Result.FileInputError;
         }
     }
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Options options = new Options();
         options.addOption("e", "echo", true, "output file contents");
+        options.addOption("h", "help", false, "print command line syntax help");
 
         CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
+        Result result;
+        try {
+            CommandLine cmd = parser.parse(options, args);
 
-        main(cmd);
+            result = main(cmd, options);
+        } catch (ParseException e) {
+            System.err.println("Wrong command line arguments, see --help for supported commands.");
+
+            result = Result.CliInputError;
+        }
+
+        System.exit(result.getCode());
+    }
+
+    public enum Result {
+        Ok(0),
+        CliInputError(1),
+        FileInputError(1);
+
+        private Result(int code) {
+            this.code = code;
+        }
+
+        private int code;
+
+        public int getCode() {
+            return code;
+        }
     }
 
 }
