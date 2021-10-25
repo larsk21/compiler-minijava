924de998cb9a31c268dc6458eb1ec2c964c25c24
larsk21 <57503246+larsk21@users.noreply.github.com>
2021-10-25 18:17:33 +0200

    add Lexer and StringTable skeletons

---
+++ src/main/java/edu/kit/compiler/lexer/Lexer.java
@@ -0,0 +1,18 @@
+package edu.kit.compiler.lexer;
+
+import edu.kit.compiler.data.Token;
+import edu.kit.compiler.io.CharCounterLookaheadIterator;
+
+public class Lexer {
+
+    public Lexer(CharCounterLookaheadIterator iterator) {}
+
+    public StringTable getStringTable() {
+        throw new RuntimeException();
+    }
+
+    public Token getNextToken() {
+        throw new RuntimeException();
+    }
+
+}
---
+++ src/main/java/edu/kit/compiler/lexer/StringTable.java
@@ -0,0 +1,13 @@
+package edu.kit.compiler.lexer;
+
+public class StringTable {
+
+    public int insert(String str) {
+        throw new RuntimeException();
+    }
+
+    public String retrieve(int hash) {
+        throw new RuntimeException();
+    }
+
+}
