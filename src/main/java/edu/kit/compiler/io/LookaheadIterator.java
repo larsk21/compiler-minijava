46501d347b960ac89f7aa70cf807a1559b691f86
larsk21 <57503246+larsk21@users.noreply.github.com>
2021-10-26 14:08:11 +0200

    add default methods to LookaheadIterator

--- src/main/java/edu/kit/compiler/io/LookaheadIterator.java
+++ src/main/java/edu/kit/compiler/io/LookaheadIterator.java
@@ -7,15 +7,30 @@ package edu.kit.compiler.io;
 public interface LookaheadIterator<T> {
 
     /**
+     * Return true if the lookahead iterator contains an element.
+     */
+    default boolean has() { return has(0); }
+
+    /**
      * Return true if the lookahead iterator contains an element at the
      * specified relative position.
      */
     boolean has(int pos);
 
     /**
+     * Return the first element.
+     */
+    default T get() { return get(0); }
+
+    /**
      * Return the element at the specified relative position.
      */
     T get(int pos);
+
+    /**
+     * Move the lookahead iterator forward one step.
+     */
+    default void next() { next(1); }
 
     /**
      * Move the lookahead iterator forward for the given number of steps.
