9593419ab3e6a67f8e037d6f543317c39bf1a7b5
Pascal Mehnert <pascalmehnert@posteo.de>
2021-10-26 20:29:30 +0200

    Fix order of expected/actual value in assertions

--- src/test/java/edu/kit/compiler/lexer/LexerTest.java
+++ src/test/java/edu/kit/compiler/lexer/LexerTest.java
@@ -20,98 +20,98 @@ public class LexerTest {
     @Test
     public void testEmptyInput() throws LexException {
         var lexer = new Lexer(getIterator(""));
-        assertEquals(lexer.getNextToken().getType(), EndOfStream);
-        assertEquals(lexer.getNextToken().getType(), EndOfStream);
+        assertEquals(EndOfStream, lexer.getNextToken().getType());
+        assertEquals(EndOfStream, lexer.getNextToken().getType());
     }
 
     @Test
     public void testDelimiters() throws LexException {
         var lexer = new Lexer(getIterator(",.:;?~()[]{}"));
-        assertEquals(lexer.getNextToken().getType(), Operator_Comma);
-        assertEquals(lexer.getNextToken().getType(), Operator_Dot);
-        assertEquals(lexer.getNextToken().getType(), Operator_Colon);
-        assertEquals(lexer.getNextToken().getType(), Operator_Semicolon);
-        assertEquals(lexer.getNextToken().getType(), Operator_Questionmark);
-        assertEquals(lexer.getNextToken().getType(), Operator_Tilde);
-        assertEquals(lexer.getNextToken().getType(), Operator_ParenL);
-        assertEquals(lexer.getNextToken().getType(), Operator_ParenR);
-        assertEquals(lexer.getNextToken().getType(), Operator_BracketL);
-        assertEquals(lexer.getNextToken().getType(), Operator_BracketR);
-        assertEquals(lexer.getNextToken().getType(), Operator_BraceL);
-        assertEquals(lexer.getNextToken().getType(), Operator_BraceR);
+        assertEquals(Operator_Comma, lexer.getNextToken().getType());
+        assertEquals(Operator_Dot, lexer.getNextToken().getType());
+        assertEquals(Operator_Colon, lexer.getNextToken().getType());
+        assertEquals(Operator_Semicolon, lexer.getNextToken().getType());
+        assertEquals(Operator_Questionmark, lexer.getNextToken().getType());
+        assertEquals(Operator_Tilde, lexer.getNextToken().getType());
+        assertEquals(Operator_ParenL, lexer.getNextToken().getType());
+        assertEquals(Operator_ParenR, lexer.getNextToken().getType());
+        assertEquals(Operator_BracketL, lexer.getNextToken().getType());
+        assertEquals(Operator_BracketR, lexer.getNextToken().getType());
+        assertEquals(Operator_BraceL, lexer.getNextToken().getType());
+        assertEquals(Operator_BraceR, lexer.getNextToken().getType());
     }
 
     @Test
     public void testArithmeticOperators() throws LexException {
         var lexer = new Lexer(getIterator("+-*/%"));
-        assertEquals(lexer.getNextToken().getType(), Operator_Plus);
-        assertEquals(lexer.getNextToken().getType(), Operator_Minus);
-        assertEquals(lexer.getNextToken().getType(), Operator_Star);
-        assertEquals(lexer.getNextToken().getType(), Operator_Slash);
-        assertEquals(lexer.getNextToken().getType(), Operator_Percent);
+        assertEquals(Operator_Plus, lexer.getNextToken().getType());
+        assertEquals(Operator_Minus, lexer.getNextToken().getType());
+        assertEquals(Operator_Star, lexer.getNextToken().getType());
+        assertEquals(Operator_Slash, lexer.getNextToken().getType());
+        assertEquals(Operator_Percent, lexer.getNextToken().getType());
     }
 
     @Test
     public void testLogicOperators() throws LexException {
         var lexer = new Lexer(getIterator("!!===>=<=><&&||"));
-        assertEquals(lexer.getNextToken().getType(), Operator_Not);
-        assertEquals(lexer.getNextToken().getType(), Operator_NotEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_EqualEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_GreaterEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_SmallerEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_Greater);
-        assertEquals(lexer.getNextToken().getType(), Operator_Smaller);
-        assertEquals(lexer.getNextToken().getType(), Operator_AndAnd);
-        assertEquals(lexer.getNextToken().getType(), Operator_BarBar);
+        assertEquals(Operator_Not, lexer.getNextToken().getType());
+        assertEquals(Operator_NotEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_EqualEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_GreaterEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_SmallerEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_Greater, lexer.getNextToken().getType());
+        assertEquals(Operator_Smaller, lexer.getNextToken().getType());
+        assertEquals(Operator_AndAnd, lexer.getNextToken().getType());
+        assertEquals(Operator_BarBar, lexer.getNextToken().getType());
     }
 
     @Test
     public void testBinaryOperators() throws LexException {
         var lexer = new Lexer(getIterator("&^|~<<>>>>>"));
-        assertEquals(lexer.getNextToken().getType(), Operator_And);
-        assertEquals(lexer.getNextToken().getType(), Operator_Circum);
-        assertEquals(lexer.getNextToken().getType(), Operator_Bar);
-        assertEquals(lexer.getNextToken().getType(), Operator_Tilde);
-        assertEquals(lexer.getNextToken().getType(), Operator_SmallerSmaller);
-        assertEquals(lexer.getNextToken().getType(), Operator_GreaterGreaterGreater);
-        assertEquals(lexer.getNextToken().getType(), Operator_GreaterGreater);
+        assertEquals(Operator_And, lexer.getNextToken().getType());
+        assertEquals(Operator_Circum, lexer.getNextToken().getType());
+        assertEquals(Operator_Bar, lexer.getNextToken().getType());
+        assertEquals(Operator_Tilde, lexer.getNextToken().getType());
+        assertEquals(Operator_SmallerSmaller, lexer.getNextToken().getType());
+        assertEquals(Operator_GreaterGreaterGreater, lexer.getNextToken().getType());
+        assertEquals(Operator_GreaterGreater, lexer.getNextToken().getType());
     }
 
     @Test
     public void testAssignmentOperators() throws LexException {
         var lexer = new Lexer(getIterator("+=-=*=/=%=&=|=^=<<=>>=>>>="));
-        assertEquals(lexer.getNextToken().getType(), Operator_PlusEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_MinusEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_StarEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_SlashEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_PercentEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_AndEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_BarEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_CircumEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_SmallerSmallerEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_GreaterGreaterEqual);
-        assertEquals(lexer.getNextToken().getType(), Operator_GreaterGreaterGreaterEqual);
+        assertEquals(Operator_PlusEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_MinusEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_StarEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_SlashEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_PercentEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_AndEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_BarEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_CircumEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_SmallerSmallerEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_GreaterGreaterEqual, lexer.getNextToken().getType());
+        assertEquals(Operator_GreaterGreaterGreaterEqual, lexer.getNextToken().getType());
     }
 
     @Test
     public void testIncDecOperators() throws LexException {
         var lexer = new Lexer(getIterator("+++---"));
-        assertEquals(lexer.getNextToken().getType(), Operator_PlusPlus);
-        assertEquals(lexer.getNextToken().getType(), Operator_Plus);
-        assertEquals(lexer.getNextToken().getType(), Operator_MinusMinus);
-        assertEquals(lexer.getNextToken().getType(), Operator_Minus);
+        assertEquals(Operator_PlusPlus, lexer.getNextToken().getType());
+        assertEquals(Operator_Plus, lexer.getNextToken().getType());
+        assertEquals(Operator_MinusMinus, lexer.getNextToken().getType());
+        assertEquals(Operator_Minus, lexer.getNextToken().getType());
     }
 
     @Test
     public void testIntegerLiteral() throws LexException {
         var lexer = new Lexer(getIterator("42"));
-        assertEquals(lexer.getNextToken(), new Token(IntegerLiteral, 1, 1, 42));
+        assertEquals(new Token(IntegerLiteral, 1, 1, 42), lexer.getNextToken());
     }
 
     @Test
     public void testZeroLiteral() throws LexException {
         var lexer = new Lexer(getIterator("0"));
-        assertEquals(lexer.getNextToken(), new Token(IntegerLiteral, 1, 1, 0));
+        assertEquals(new Token(IntegerLiteral, 1, 1, 0), lexer.getNextToken());
     }
 
     @Test
