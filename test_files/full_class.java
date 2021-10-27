c3040d5dedcbe6b481618e7b8c721b99906d953e
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-10-27 17:36:46 +0200

    test file for parser

---
+++ test_files/full_class.java
@@ -0,0 +1,14 @@
+class Test {
+	public int x;
+	public ctype[][] z;
+
+	public void m1() {}
+
+	public void m2(int i, boolean b) throws Exception {}
+
+	public static void main(String[] args) {
+		int i = 1;
+		j = i + (i * 2);
+		int[][] array = new int[5][][2];
+	}
+}
