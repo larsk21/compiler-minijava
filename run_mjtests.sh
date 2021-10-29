745b7d413f3bb21230b4bab11a5631afabfc0a7c
0xNB <niklasbetten98@gmail.com>
2021-10-29 19:49:56 +0200

    automatic mjtest setup

---
+++ run_mjtests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ..
+
+echo "Reading User Path and running mjtests"
+
+export PATH=$(cat ./compiler-minijava/userpath)
+export JAVA_HOME=$(cat ./compiler-minijava/userhome)
+export MJ_TIMEOUT=10
+export MJ_RUN='./compiler-minijava/run'
+./mjtest/mjt.py --log_level  error -ci_testing --parallel lexer
