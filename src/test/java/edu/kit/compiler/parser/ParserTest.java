642d5ce2d65af12b8f77e013d2d4f26e0a698702
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-11-03 16:53:40 +0100

    some more parser tests

--- src/test/java/edu/kit/compiler/parser/ParserTest.java
+++ src/test/java/edu/kit/compiler/parser/ParserTest.java
@@ -16,6 +16,25 @@ public class ParserTest {
         parser.parse();
     }
 
+    @Test
+    public void testAttributes() {
+        var parser = new Parser(new Lexer(getIterator(
+            "class Test {public int x; public ctype[][] z;}"
+        )));
+        parser.parse();
+    }
+
+    @Test
+    public void testMethods() {
+        var parser = new Parser(new Lexer(getIterator(
+            "class Test {"
+            + "public void m1() {}"
+            + "public void m2(int i, int j) {}"
+            + "public static void main(String[] args){} }"
+        )));
+        parser.parse();
+    }
+
     private static ReaderCharIterator getIterator(String input) {
         return getIterator(new StringReader(input));
     }
