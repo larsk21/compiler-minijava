14598037c27a208622435bf965d4e8397f1876ee
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-11-09 10:33:07 +0100

    basic test for ast with pretty printer

--- src/test/java/edu/kit/compiler/parser/ParserTest.java
+++ src/test/java/edu/kit/compiler/parser/ParserTest.java
@@ -1,12 +1,18 @@
 package edu.kit.compiler.parser;
 
+import edu.kit.compiler.data.ast_nodes.ClassNode;
 import edu.kit.compiler.io.ReaderCharIterator;
 import edu.kit.compiler.lexer.Lexer;
+import edu.kit.compiler.lexer.StringTable;
+
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.io.Reader;
 import java.io.StringReader;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -50,6 +56,43 @@ public class ParserTest {
                 "class int {}"
         )));
         assertThrows(ParseException.class, (parser)::parse, "not part of language spec");
+    }
+
+    @Test
+    public void testSimpleAst() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream sysout = System.out;
+        System.setOut(new PrintStream(stream));
+
+        Lexer lexer = new Lexer(getIterator(
+            "class Test {"
+            + "public int i;"
+            + "public static void main(String[] args){"
+            + "  int j = i + 1 - j;"
+            + "} }"
+        ));
+
+        StringTable stringTable = lexer.getStringTable();
+        PrettyPrintAstVisitor visitor = new PrettyPrintAstVisitor(stringTable);
+
+        var parser = new Parser(lexer);
+        var classes = parser.parse();
+
+        for (ClassNode node: classes) {
+            node.accept(visitor);
+        }
+        String result = stream.toString();
+
+        assertEquals(
+            "class Test {\n"
+            + "\tpublic static void main(String[] args) {\n"
+            + "\t\tint j = (i + 1) - j;\n"
+            + "\t}\n"
+            + "\tpublic int i;\n"
+            + "}",
+            result
+        );
+        System.setOut(sysout);
     }
 
     private static ReaderCharIterator getIterator(String input) {
