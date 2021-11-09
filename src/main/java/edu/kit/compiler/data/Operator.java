293134a0a607aa0ba2761a0c6e868ee42e11cdaa
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-11-09 09:55:55 +0100

    binary operator from token

--- src/main/java/edu/kit/compiler/data/Operator.java
+++ src/main/java/edu/kit/compiler/data/Operator.java
@@ -1,5 +1,7 @@
 package edu.kit.compiler.data;
 
+import edu.kit.compiler.parser.ParseException;
+
 /**
  * Represents the supported operators in MiniJava.
  */
@@ -24,7 +26,42 @@ public final class Operator {
         Subtraction,
         Multiplication,
         Division,
-        Modulo
+        Modulo;
+
+        public static BinaryOperator fromToken(Token token) {
+            switch(token.getType()) {
+                case Operator_Equal:
+                    return BinaryOperator.Assignment;
+                case Operator_BarBar:
+                    return BinaryOperator.LogicalOr;
+                case Operator_AndAnd:
+                    return BinaryOperator.LogicalAnd;
+                case Operator_EqualEqual:
+                    return BinaryOperator.Equal;
+                case Operator_NotEqual:
+                    return BinaryOperator.NotEqual;
+                case Operator_Smaller:
+                    return BinaryOperator.LessThan;
+                case Operator_SmallerEqual:
+                    return BinaryOperator.LessThanOrEqual;
+                case Operator_Greater:
+                    return BinaryOperator.GreaterThan;
+                case Operator_GreaterEqual:
+                    return BinaryOperator.GreaterThanOrEqual;
+                case Operator_Plus:
+                    return BinaryOperator.Addition;
+                case Operator_Minus:
+                    return BinaryOperator.Subtraction;
+                case Operator_Star:
+                    return BinaryOperator.Multiplication;
+                case Operator_Slash:
+                    return BinaryOperator.Division;
+                case Operator_Percent:
+                    return BinaryOperator.Modulo;
+                default:
+                    throw new ParseException(token, "expected binary operator");
+            }
+        }
     }
 
     /**
