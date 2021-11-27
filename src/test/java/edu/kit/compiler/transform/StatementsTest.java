d9a4ae48360722b46ec82b7eaa8ad12ca3cdc778
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-11-27 12:04:23 +0100

    tests for basic statements

---
+++ src/test/java/edu/kit/compiler/transform/StatementsTest.java
@@ -0,0 +1,78 @@
+package edu.kit.compiler.transform;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+
+import edu.kit.compiler.data.ast_nodes.ClassNode;
+import edu.kit.compiler.data.ast_nodes.MethodNode;
+import edu.kit.compiler.data.ast_nodes.ProgramNode;
+import edu.kit.compiler.data.ast_nodes.MethodNode.DynamicMethodNode;
+import edu.kit.compiler.lexer.Lexer;
+import edu.kit.compiler.logger.Logger;
+import edu.kit.compiler.parser.Parser;
+import edu.kit.compiler.semantic.DetailedNameTypeAstVisitor;
+import edu.kit.compiler.semantic.ErrorHandler;
+import edu.kit.compiler.semantic.NamespaceGatheringVisitor;
+import edu.kit.compiler.semantic.NamespaceMapper;
+
+public class StatementsTest {
+    private static final String main = "class Main {public static void main(String[] args) {}}";
+    private TypeMapper typeMapper;
+
+    private TransformContext initContext(String input) {
+        JFirmSingleton.initializeFirmLinux();
+
+        ErrorHandler errorHandler = new ErrorHandler(Logger.nullLogger());
+        Lexer lexer = new Lexer(getReader(input));
+        ProgramNode node =  new Parser(lexer).parse();
+
+        NamespaceMapper namespaceMapper = new NamespaceMapper();
+        NamespaceGatheringVisitor visitor = new NamespaceGatheringVisitor(
+            namespaceMapper, lexer.getStringTable(), errorHandler
+        );
+        node.accept(visitor);
+        DetailedNameTypeAstVisitor dntv = new DetailedNameTypeAstVisitor(namespaceMapper, lexer.getStringTable());
+        node.accept(dntv);
+        this.typeMapper = new TypeMapper(namespaceMapper, lexer.getStringTable());
+        for (ClassNode classNode: node.getClasses()) {
+            for (DynamicMethodNode m: classNode.getDynamicMethods()) {
+                var mapping = LocalVariableCounter.apply(m);
+                return new TransformContext(
+                    typeMapper, classNode, m, mapping, mapping.size(), false
+                );
+            }
+        }
+        throw new IllegalArgumentException("Class contains no method.");
+    }
+
+    @Test
+    public void testEmpty() throws IOException {
+        var context = initContext("class c { public void m() { } }" + main);
+        Statements.apply(context);
+    }
+
+    @Test
+    public void testWithReturn() throws IOException {
+        var context = initContext("class c { public int m() { int i; return i; } }" + main);
+        Statements.apply(context);
+    }
+
+    @Test
+    public void testWithRefReturn() throws IOException {
+        var context = initContext("class c { public c m() { c ref; return ref; } }" + main);
+        Statements.apply(context);
+    }
+
+    @Test
+    public void testWithParReturn() throws IOException {
+        var context = initContext("class c { public int m(int x) { return x; } }" + main);
+        Statements.apply(context);
+    }
+
+    private static Reader getReader(String input) {
+        return new StringReader(input);
+    }
+}
