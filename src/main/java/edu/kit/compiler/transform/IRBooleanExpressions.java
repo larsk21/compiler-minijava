659f116f2ab6e21a37b5a27c0cfc6c9cc1b08793
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-11-29 23:17:31 +0100

    implement boolean values

--- src/main/java/edu/kit/compiler/transform/IRBooleanExpressions.java
+++ src/main/java/edu/kit/compiler/transform/IRBooleanExpressions.java
@@ -2,6 +2,8 @@ package edu.kit.compiler.transform;
 
 
 import edu.kit.compiler.data.AstVisitor;
+import edu.kit.compiler.data.DataType;
+import edu.kit.compiler.data.DataType.DataTypeClass;
 import edu.kit.compiler.data.Operator.UnaryOperator;
 import edu.kit.compiler.data.ast_nodes.ExpressionNode;
 import edu.kit.compiler.data.ast_nodes.ExpressionNode.ArrayAccessExpressionNode;
@@ -21,13 +23,86 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class IRBooleanExpressions {
+    private static final DataType boolType = new DataType(DataTypeClass.Boolean);
+
     public static Node asValue(TransformContext context, ExpressionNode expr) {
-        throw new UnsupportedOperationException();
+        if (!expr.getResultType().equals(boolType)) {
+            throw new IllegalArgumentException();
+        }
+        return expr.accept(new ValueVisitor(context));
     }
 
     public static void asConditional(TransformContext context, ExpressionNode expr,
                                      Block trueBranch, Block falseBranch) {
+        if (!expr.getResultType().equals(boolType)) {
+            throw new IllegalArgumentException();
+        }
         expr.accept(new ConditionalVisitor(context, trueBranch, falseBranch));
+    }
+
+    @AllArgsConstructor
+    private static class ValueVisitor implements AstVisitor<Node> {
+        private TransformContext context;
+
+        public Node visit(BinaryExpressionNode expr) { 
+            return fromConditional(expr);
+        }
+
+        public Node visit(UnaryExpressionNode expr) {
+            if (expr.getOperator() != UnaryOperator.LogicalNegation) {
+                throw new IllegalArgumentException();
+            }
+
+            /**
+             * We try to create some optimized code here, because
+             * optimizing conditionals/branches afterwards is not that simple.
+             */
+            ExpressionNode inner = expr.getExpression();
+            if (inner instanceof BinaryExpressionNode) {
+                return fromConditional(expr);
+            } else if (inner instanceof UnaryExpressionNode) {
+                return evalExpression(context, ((UnaryExpressionNode)inner).getExpression());
+            } else {
+                // calculate inverse via XOR
+                // This assumes that a boolean one is always the integer '1'!
+                Construction con = context.getConstruction();
+                Node value = evalExpression(context, expr);
+                Node constOne = con.newConst(1, Mode.getBu());
+                return con.newEor(value, constOne);
+            }
+        }
+
+        public Node visit(ValueExpressionNode expr) {
+            Construction con = context.getConstruction();
+            return switch (expr.getValueType()) {
+                case True -> con.newConst(1, Mode.getBu());
+                case False -> con.newConst(0, Mode.getBu());
+                default -> throw new IllegalStateException();
+            };
+        }
+
+        private Node fromConditional(ExpressionNode expr) {
+            // manual phi (possibly)
+            Construction con = context.getConstruction();
+            Block trueBranch = con.newBlock();
+            Block falseBranch = con.newBlock();
+            IRBooleanExpressions.asConditional(context, expr, trueBranch, falseBranch);
+
+            trueBranch.mature();
+            con.setCurrentBlock(trueBranch);
+            Node constOne = con.newConst(1, Mode.getBu());
+            Node jmpA = con.newJmp();
+            falseBranch.mature();
+            con.setCurrentBlock(falseBranch);
+            Node constZero = con.newConst(0, Mode.getBu());
+            Node jmpB = con.newJmp();
+
+            Block finalBlock = con.newBlock();
+            finalBlock.addPred(trueBranch);
+            finalBlock.addPred(falseBranch);
+            finalBlock.mature();
+            return con.newPhi(new Node[] {trueBranch, falseBranch}, Mode.getBu());
+        }
     }
 
     @AllArgsConstructor
