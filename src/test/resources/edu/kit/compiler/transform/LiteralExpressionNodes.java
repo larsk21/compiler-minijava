1abd7606ae64e28e0228657710b0ae21ae0472cf
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-03 16:18:03 +0100

    cleanup / fixes for expression visitor

--- src/test/resources/edu/kit/compiler/transform/LiteralExpressionNodes.java
+++
@@ -1,9 +0,0 @@
-class Sieve {
-    public int isPrimeLength;
-    public boolean[] isPrime;
-
-    public static void main(String[] args) {
-        int test = 2;
-        Sieve sieve = null;
-    }
-}
