5c52f42d56ce7731ca8c41c732ef68112a32b470
Pascal Mehnert <pascalmehnert@posteo.de>
2021-12-04 21:01:14 +0100

    Introduce virtual main method to set exit status

--- src/main/java/edu/kit/compiler/transform/Lower.java
+++ src/main/java/edu/kit/compiler/transform/Lower.java
@@ -2,11 +2,8 @@ package edu.kit.compiler.transform;
 
 import com.sun.jna.Platform;
 import firm.*;
+import firm.nodes.Call;
 import firm.nodes.Node;
-import firm.Backend;
-import firm.Ident;
-import firm.Program;
-import firm.Util;
 import firm.bindings.binding_typerep.ir_visibility;
 
 public class Lower {
@@ -19,6 +16,7 @@ public class Lower {
 
     public static void lower(TypeMapper typeMapper) {
         lowerMethods(typeMapper);
+        setMainMethod(typeMapper);
         Backend.lowerForTarget();
         Util.lowerSels();
     }
@@ -40,11 +38,38 @@ public class Lower {
                 method.setOwner(globalType);
             }
         }
+    }
 
-        // Rename main method, so gcc recognizes it
-        var main = typeMapper.getMainMethod();
-        main.setLdIdent(Ident.mangleGlobal("main"));
-        main.setVisibility(ir_visibility.ir_visibility_external);
-        Program.setMainGraph(main.getGraph());
+    /**
+     * Generates a virtual main method that wraps the user defined main method.
+     * The generated method always return zero to ensure that compiled programs
+     * always exit with status 0.
+     */
+    private static void setMainMethod(TypeMapper typeMapper) {
+        var global = Program.getGlobalType();
+        var mainType = new MethodType(new Type[] {}, new Type[] { new PrimitiveType(Mode.getIs())} );
+        var mainEntity = new Entity(global, Ident.mangleGlobal("main"), mainType);
+        mainEntity.setVisibility(ir_visibility.ir_visibility_external);
+
+        var graph = new Graph(mainEntity, 0);
+        var cons = new Construction(graph);
+
+        // call user-defined main method
+        var call = cons.newCall(cons.getCurrentMem(),
+            cons.newAddress(typeMapper.getMainMethod()),
+            new Node[] { cons.newConst(0, Mode.getP()) },
+            typeMapper.getMainMethod().getType()
+        );
+        cons.setCurrentMem(cons.newProj(call, Mode.getM(), Call.pnM));
+        
+        // Set exit status (= return value) to zero
+        var ret = cons.newReturn(cons.getCurrentMem(),
+            new Node[] { cons.newConst(0, Mode.getIs()) }
+        );
+
+        graph.getEndBlock().addPred(ret);
+        cons.finish();
+
+        Program.setMainGraph(graph);
     }
 }
