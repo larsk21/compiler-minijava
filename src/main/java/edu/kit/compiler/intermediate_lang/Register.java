51101fa0f697ece809518ad3a2f4c2702a6c1f37
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-09 01:35:10 +0100

    enum for registers

---
+++ src/main/java/edu/kit/compiler/intermediate_lang/Register.java
@@ -0,0 +1,24 @@
+package edu.kit.compiler.intermediate_lang;
+
+/**
+ * Represents a concrete x86 register.
+ * TODO: String representation, byte width?
+ */
+public enum Register {
+    RAX,
+    RBX,
+    RCX,
+    RDX,
+    RSI,
+    RDI,
+    RBP,
+    RSP,
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    R13,
+    R14,
+    R15,
+}
