bf42d761349da0695ef9a6221802e2bbe44ef704
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-09 22:32:29 +0100

    assignment

---
+++ src/main/java/edu/kit/compiler/register_allocation/RegisterAssignment.java
@@ -0,0 +1,31 @@
+package edu.kit.compiler.register_allocation;
+
+import edu.kit.compiler.intermediate_lang.Register;
+import lombok.Getter;
+
+import java.util.Optional;
+
+/**
+ * A virtual register can either be assigned to a concrete register
+ * or to a stack slot (represented by the offset to the base pointer).
+ */
+public class RegisterAssignment {
+    @Getter
+    private Optional<Register> register;
+    @Getter
+    private Optional<Integer> stackSlot;
+
+    public RegisterAssignment(Register cRegister) {
+        this.register = Optional.of(cRegister);
+        this.stackSlot = Optional.empty();
+    }
+
+    public RegisterAssignment(int stackSlot) {
+        this.register = Optional.empty();
+        this.stackSlot = Optional.of(stackSlot);
+    }
+
+    public boolean isSpilled() {
+        return stackSlot.isPresent();
+    }
+}
