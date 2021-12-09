db931d9eba7ae5bc2a85fae1c232a136092f110d
Pascal Mehnert <pascalmehnert@posteo.de>
2021-12-09 15:56:04 +0100

    Add and improve test cases

--- mjtest-files/compile-firm/arithmethic-identities/MinusSub.java
+++ mjtest-files/compile-firm/arithmethic-identities/MinusSub.java
@@ -3,9 +3,16 @@ class MinusSub {
         MinusSub m = new MinusSub();
 
         System.out.println(m.simple(42, 28));
+
+        System.out.println(m.moreComplex(21, 21));
+        System.out.println(m.moreComplex(-1024, 2048));
     }
 
     public int simple(int x, int y) {
         return -(x - y);
+    }
+
+    public int moreComplex(int x, int y) {
+        return -(-x - y);  /* -->  x + y */
     }
 }
