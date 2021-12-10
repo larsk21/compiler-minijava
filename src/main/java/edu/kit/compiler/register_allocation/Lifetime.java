3fcb5772354434ed184792da9bdee15c7b082e82
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-10 10:53:27 +0100

    refined lifetime

--- src/main/java/edu/kit/compiler/register_allocation/Lifetime.java
+++ src/main/java/edu/kit/compiler/register_allocation/Lifetime.java
@@ -11,10 +11,22 @@ public class Lifetime {
     @Getter
     private int end;
 
-    public Lifetime(int begin, int end) {
+    /**
+     * Whether the register is an input register for the last instruction of the lifetime.
+     * Note that for the first instruction, the register always is the target register
+     * (due to dominance requirements).
+     */
+    @Getter
+    private boolean lastInstrIsInput;
+
+    public Lifetime(int begin, int end, boolean lastInstrIsInput) {
         assert begin < end;
         this.begin = begin;
         this.end = end;
+        this.lastInstrIsInput = lastInstrIsInput;
+        if (lastInstrIsInput) {
+            assert begin + 1 < end;
+        }
     }
 
     public boolean contains(int time) {
@@ -22,6 +34,13 @@ public class Lifetime {
     }
 
     public boolean interferes(Lifetime other) {
+        // check cases where one register is allowed to replace the other
+        // within the same instruction
+        if (other.begin + 1 == end) {
+            return lastInstrIsInput;
+        } else if (begin + 1 == other.end) {
+            return other.lastInstrIsInput;
+        }
         return begin < other.end && other.begin < end;
     }
 }
