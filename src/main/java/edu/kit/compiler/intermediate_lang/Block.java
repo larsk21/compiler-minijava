d9a6b9927aeb3203432fe1654026183cb99fa41c
Niklas Betten <niklasbetten98@gmail.com>
2021-12-11 22:31:23 +0100

    data structures for blocks and program

--- src/main/java/edu/kit/compiler/intermediate_lang/Block.java
+++ src/main/java/edu/kit/compiler/intermediate_lang/Block.java
@@ -7,7 +7,19 @@ import java.util.List;
 @Data
 public class Block {
 
-    private List<Instruction> instructions;
+    /**
+     * List of instructions that this block contains, the last instruction has to be the jump instruction
+     */
+    private final List<Instruction> instructions;
+    private final List<Block> targets;
+    private final List<Block> references;
 
+    public Block(List<Instruction> instructions, List<Block> targets, List<Block> references) {
+        // block has to contain at least one jump instruction
+        assert instructions.size() > 1;
+        this.instructions = instructions;
+        this.targets = targets;
+        this.references = references;
+    }
 
 }
