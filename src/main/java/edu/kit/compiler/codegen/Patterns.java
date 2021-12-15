95e5bfd4129c660bd249c2c582b10ab7c08eb46c
Pascal Mehnert <pascalmehnert@posteo.de>
2021-12-15 22:38:13 +0100

    Rework pattern collection

--- src/main/java/edu/kit/compiler/codegen/Patterns.java
+++ src/main/java/edu/kit/compiler/codegen/Patterns.java
@@ -1,23 +1,7 @@
 package edu.kit.compiler.codegen;
 
-import static firm.bindings.binding_irnode.ir_opcode.iro_Add;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Block;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Call;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Cmp;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Const;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Conv;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Div;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Eor;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Load;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Minus;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Mod;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Mul;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Phi;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Return;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Store;
-import static firm.bindings.binding_irnode.ir_opcode.iro_Sub;
+import static firm.bindings.binding_irnode.ir_opcode.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +11,7 @@ import edu.kit.compiler.codegen.pattern.BinaryInstructionPattern;
 import edu.kit.compiler.codegen.pattern.BlockPattern;
 import edu.kit.compiler.codegen.pattern.CallPattern;
 import edu.kit.compiler.codegen.pattern.ComparisonPattern;
+import edu.kit.compiler.codegen.pattern.ConditionPattern;
 import edu.kit.compiler.codegen.pattern.ConversionPattern;
 import edu.kit.compiler.codegen.pattern.DivisionPattern;
 import edu.kit.compiler.codegen.pattern.DivisionPattern.Type;
@@ -41,18 +26,74 @@ import edu.kit.compiler.codegen.pattern.ReturnPattern;
 import edu.kit.compiler.codegen.pattern.UnaryInstructionPattern;
 import firm.bindings.binding_irnode.ir_opcode;
 import firm.nodes.Node;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 
 public class Patterns implements Pattern<InstructionMatch> {
 
     private static final Pattern<OperandMatch<Register>> REGISTER = OperandPattern.register();
     private static final Pattern<OperandMatch<Memory>> MEMORY = OperandPattern.memory();
 
-    private final Map<ir_opcode, List<Pattern<InstructionMatch>>> map;
+    private final Map<ir_opcode, Pattern<InstructionMatch>> map;
 
     @Override
     public InstructionMatch match(Node node, MatcherState matcher) {
-        var patterns = map.get(node.getOpCode());
-        if (patterns != null) {
+        var pattern = map.get(node.getOpCode());
+        if (pattern != null) {
+            return pattern.match(node, matcher);
+        } else {
+            return InstructionMatch.none();
+        }
+    }
+
+    public Patterns() {
+        map = Map.ofEntries(
+                Map.entry(iro_Const, new LoadImmediatePattern()),
+                Map.entry(iro_Add, new BinaryInstructionPattern(iro_Add, "add", REGISTER, REGISTER, true, false)),
+                Map.entry(iro_Sub, new BinaryInstructionPattern(iro_Sub, "sub", REGISTER, REGISTER, true, false)),
+                Map.entry(iro_Mul, new BinaryInstructionPattern(iro_Mul, "imul", REGISTER, REGISTER, true, false)),
+                Map.entry(iro_Eor, new BinaryInstructionPattern(iro_Eor, "xor", REGISTER, REGISTER, true, false)),
+
+                Map.entry(iro_Div, new DivisionPattern(Type.DIV, REGISTER, REGISTER)),
+                Map.entry(iro_Mod, new DivisionPattern(Type.MOD, REGISTER, REGISTER)),
+
+                Map.entry(iro_Minus, new UnaryInstructionPattern(iro_Minus, "neg", REGISTER, true, false)),
+
+                Map.entry(iro_Cmp, new ComparisonPattern()),
+
+                Map.entry(iro_Conv, new ConversionPattern()),
+
+                Map.entry(iro_Store, new BinaryInstructionPattern(iro_Store, "mov", MEMORY, REGISTER, false, true)),
+                Map.entry(iro_Load, new LoadMemoryPattern()),
+
+                Map.entry(iro_Call, new CallPattern()),
+                Map.entry(iro_Phi, new PhiPattern()),
+                Map.entry(iro_Block, new BlockPattern()),
+                Map.entry(iro_Return, new ReturnPattern()),
+
+                Map.entry(iro_Jmp, new ConditionPattern()),
+                Map.entry(iro_Cond, new ConditionPattern()),
+
+                // trivial pattern for nodes without predecessors
+                Map.entry(iro_Start, new EmptyPattern()),
+                Map.entry(iro_End, new EmptyPattern()),
+                Map.entry(iro_Address, new EmptyPattern()));
+    }
+
+    private static final class EmptyPattern implements Pattern<InstructionMatch> {
+        @Override
+        public InstructionMatch match(Node node, MatcherState matcher) {
+            return InstructionMatch.empty(node);
+        }
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    private static final class CompoundPattern implements Pattern<InstructionMatch> {
+
+        private final List<Pattern<InstructionMatch>> patterns;
+
+        @Override
+        public InstructionMatch match(Node node, MatcherState matcher) {
             for (var pattern : patterns) {
                 var shadow = matcher.getShadow();
                 var match = pattern.match(node, shadow);
@@ -61,40 +102,8 @@ public class Patterns implements Pattern<InstructionMatch> {
                     return match;
                 }
             }
+            return InstructionMatch.none();
         }
-        return InstructionMatch.none();
-    }
 
-    public Patterns() {
-        map = Map.ofEntries(
-                Map.entry(iro_Const, Arrays.asList(new LoadImmediatePattern())),
-                Map.entry(iro_Add, Arrays.asList(
-                        new BinaryInstructionPattern(iro_Add, "add", REGISTER, REGISTER, true, false))),
-                Map.entry(iro_Sub, Arrays.asList(
-                        new BinaryInstructionPattern(iro_Sub, "sub", REGISTER, REGISTER, true, false))),
-                Map.entry(iro_Mul, Arrays.asList(
-                        new BinaryInstructionPattern(iro_Mul, "imul", REGISTER, REGISTER, true, false))),
-                Map.entry(iro_Eor, Arrays.asList(
-                        new BinaryInstructionPattern(iro_Eor, "xor", REGISTER, REGISTER, true, false))),
-
-                Map.entry(iro_Minus, Arrays.asList(
-                        new UnaryInstructionPattern(iro_Minus, "neg", REGISTER, true, false))),
-
-                Map.entry(iro_Cmp, Arrays.asList(new ComparisonPattern())),
-
-                Map.entry(iro_Conv, Arrays.asList(new ConversionPattern())),
-
-                Map.entry(iro_Store, Arrays.asList(
-                        new BinaryInstructionPattern(iro_Store, "mov", MEMORY, REGISTER, false, true))),
-                Map.entry(iro_Load, Arrays.asList(new LoadMemoryPattern())),
-
-                Map.entry(iro_Div, Arrays.asList(new DivisionPattern(Type.DIV, REGISTER, REGISTER))),
-                Map.entry(iro_Mod, Arrays.asList(new DivisionPattern(Type.MOD, REGISTER, REGISTER))),
-
-                Map.entry(iro_Call, Arrays.asList(new CallPattern())),
-                Map.entry(iro_Phi, Arrays.asList(new PhiPattern())),
-                Map.entry(iro_Block, Arrays.asList(new BlockPattern())),
-                Map.entry(iro_Return, Arrays.asList(new ReturnPattern()))
-        );
     }
 }
