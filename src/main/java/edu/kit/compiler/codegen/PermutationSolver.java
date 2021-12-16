d8fd44108d70d72359747fbfbce398e70fcd4010
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-16 22:44:07 +0100

    extend solver to handle overlapping inputs

--- src/main/java/edu/kit/compiler/codegen/PermutationSolver.java
+++ src/main/java/edu/kit/compiler/codegen/PermutationSolver.java
@@ -2,20 +2,27 @@ package edu.kit.compiler.codegen;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PermutationSolver {
-    private Map<Integer, Integer> mapping;
+    private Map<Integer, Deque<Integer>> mapping;
 
     public PermutationSolver() {
         this.mapping = new HashMap<>();
     }
 
     public void addMapping(int input, int target) {
-        mapping.put(input, target);
+        if (mapping.containsKey(input)) {
+            mapping.get(input).push(target);
+        } else {
+            Deque<Integer> vals = new ArrayDeque<>();
+            vals.push(target);
+            mapping.put(input, vals);
+        }
+    }
+
+    public void addMapping(Assignment assignment) {
+        addMapping(assignment.getInput(), assignment.getTarget());
     }
 
     /**
@@ -33,32 +40,63 @@ public class PermutationSolver {
         while (!mapping.isEmpty()) {
             // each iteration handles one "connected component"
             assignOrder.clear();
+
+            int firstInput = -1;
             int currentTarget = -1;
-            for (Map.Entry<Integer, Integer> entry: mapping.entrySet()) {
-                currentTarget = entry.getValue();
-                assignOrder.add(entry.getKey());
+            Deque<Integer> currentVals = null;
+            for (var entry: mapping.entrySet()) {
+                currentVals = entry.getValue();
+                firstInput = entry.getKey();
+                currentTarget = currentVals.pop();
+                assignOrder.add(firstInput);
                 break;
             }
+
+            boolean cycle = false;
             while (mapping.containsKey(currentTarget)) {
-                if (currentTarget == assignOrder.get(0) && assignOrder.size() > 1) {
-                    // handle cycle
-                    assignOrder.add(0, temporary);
-                    currentTarget = temporary;
-                    break;
-                } else if (currentTarget == assignOrder.get(0)) {
-                    // self-assignment
-                    mapping.remove(currentTarget);
-                    assignOrder.clear();
+                if (currentTarget == firstInput) {
+                    if (assignOrder.size() > 1) {
+                        // handle cycle
+                        assignOrder.add(0, temporary);
+                        mapping.put(firstInput, currentVals);
+                        currentTarget = temporary;
+                    } else {
+                        // self-assignment
+                        if (currentVals.isEmpty()) {
+                            mapping.remove(firstInput);
+                        }
+                        assignOrder.clear();
+                    }
+                    cycle = true;
                     break;
                 }
+
+                // if more assignments are remaining for this input,
+                // we propagate them to the target so that they are
+                // processed in another round
+                Deque<Integer> tmp = currentVals;
+                currentVals = mapping.get(currentTarget);
+                mapping.put(currentTarget, tmp);
                 assignOrder.add(currentTarget);
-                currentTarget = mapping.get(currentTarget);
+                currentTarget = currentVals.pop();
             }
+
+            // update mapping for beginning and end if there was no cycle
+            if (!cycle) {
+                mapping.remove(firstInput);
+                if (!currentVals.isEmpty()) {
+                    mapping.put(currentTarget, currentVals);
+                }
+            }
+
+            // output moves and cleanup
             for (int i = assignOrder.size() - 1; i >= 0; i--) {
                 int prev = assignOrder.get(i);
                 result.add(new Assignment(prev, currentTarget));
                 currentTarget = prev;
-                mapping.remove(prev);
+                if (mapping.containsKey(prev) && mapping.get(prev).isEmpty()) {
+                    mapping.remove(prev);
+                }
             }
         }
 
--- src/test/java/edu/kit/compiler/codegen/PermutationSolverTest.java
+++ src/test/java/edu/kit/compiler/codegen/PermutationSolverTest.java
@@ -19,9 +19,43 @@ public class PermutationSolverTest {
         }
     }
 
+    @Test
+    public void testSwap() {
+        List<Integer> input = List.of(0, 1);
+        List<Integer> targets = List.of(1, 0);
+        checkForData(input, targets, 2);
+    }
+
+    @Test
+    public void testCycle() {
+        List<Integer> input = List.of(0, 1, 2);
+        List<Integer> targets = List.of(2, 0, 1);
+        checkForData(input, targets, 3);
+    }
+
+    @Test
+    public void testTree() {
+        List<Integer> input = List.of(0, 1, 0, 1);
+        List<Integer> targets = List.of(1, 3, 2, 4);
+        checkForData(input, targets, 5);
+    }
+
+    @Test
+    public void testCycleWithLeafs1() {
+        List<Integer> input = List.of(0, 1, 2, 0, 1, 2, 0, 1, 2);
+        List<Integer> targets = List.of(1, 2, 0, 3, 4, 5, 6, 7, 8);
+        checkForData(input, targets, 9);
+    }
+
+    @Test
+    public void testCycleWithLeafs2() {
+        List<Integer> input = List.of(0, 1, 2, 0, 1, 2, 0, 1, 2);
+        List<Integer> targets = List.of(3, 4, 5, 6, 7, 8, 1, 2, 0);
+        checkForData(input, targets, 9);
+    }
+
     @RepeatedTest(100)
     public void testRandomized() {
-        PermutationSolver solver = new PermutationSolver();
         int size = (int)(10 * Math.random());
         int inputRange = size + (int)(3 * Math.random());
         List<Integer> input = new ArrayList<>();
@@ -43,19 +77,47 @@ public class PermutationSolverTest {
             targets.add(next);
         }
 
+        checkForData(input, targets, inputRange);
+    }
+
+    @RepeatedTest(100)
+    public void testRandomizedOverlappingInput() {
+        int size = (int)(10 * Math.random()) + 2;
+        List<Integer> input = new ArrayList<>();
+        List<Integer> targets = new ArrayList<>();
+
         for (int i = 0; i < size; i++) {
+            input.add((int)(size * Math.random()) / 2);
+        }
+
+        for (int i = 0; i < size; i++) {
+            int next;
+            do {
+                next = (int)(size * Math.random());
+            } while (targets.contains(next));
+            targets.add(next);
+        }
+
+        checkForData(input, targets, size);
+    }
+
+    private void checkForData(List<Integer> input, List<Integer> targets, int maxInput) {
+        assertEquals(input.size(), targets.size());
+        PermutationSolver solver = new PermutationSolver();
+
+        for (int i = 0; i < input.size(); i++) {
             solver.addMapping(input.get(i), targets.get(i));
         }
 
-        int[] data = new int[inputRange + 1];
-        for (int i = 0; i <= inputRange; i++) {
+        int[] data = new int[maxInput + 1];
+        for (int i = 0; i <= maxInput; i++) {
             data[i] = i;
         }
-        for (var ass: solver.solve(inputRange)) {
+        for (var ass: solver.solve(maxInput)) {
             data[ass.getTarget()] = data[ass.getInput()];
         }
-        for (int i = 0; i < size; i++) {
-            assertEquals(input.get(i), data[targets.get(i)]);
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(input.get(i), data[targets.get(i)], "index=" + i);
         }
     }
 }
