a40202f6ca8da2f90201a995db729fc0a385657b
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-17 08:29:06 +0100

    Typo

--- src/test/java/edu/kit/compiler/register_allocation/DumbAllocatorTest.java
+++ src/test/java/edu/kit/compiler/register_allocation/DumbAllocatorTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DumpAllocatorTest {
+public class DumbAllocatorTest {
 
     @Test
     public void testCompareToFirm() {
