6fff9f71c1128397b40ebc07fa167b5b837e43ad
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-12-23 16:02:56 +0100

    implement "compile" command

--- src/main/java/edu/kit/compiler/JavaEasyCompiler.java
+++ src/main/java/edu/kit/compiler/JavaEasyCompiler.java
@@ -1,16 +1,24 @@
 package edu.kit.compiler;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.Reader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
+import edu.kit.compiler.assembly.AssemblyWriter;
+import edu.kit.compiler.assembly.ElfAssemblyWriter;
+import edu.kit.compiler.assembly.FunctionInstructions;
+import edu.kit.compiler.codegen.InstructionSelection;
+import edu.kit.compiler.codegen.PatternCollection;
+import edu.kit.compiler.codegen.PhiResolver;
+import edu.kit.compiler.codegen.ReversePostfixOrder;
+import edu.kit.compiler.intermediate_lang.Block;
+import edu.kit.compiler.intermediate_lang.RegisterSize;
+import edu.kit.compiler.register_allocation.DumbAllocator;
+import edu.kit.compiler.register_allocation.RegisterAllocator;
 import edu.kit.compiler.transform.IRVisitor;
+import firm.*;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -38,11 +46,6 @@ import edu.kit.compiler.semantic.NamespaceMapper;
 import edu.kit.compiler.semantic.SemanticChecks;
 import edu.kit.compiler.transform.JFirmSingleton;
 import edu.kit.compiler.transform.Lower;
-
-import firm.Backend;
-import firm.Firm;
-import firm.Graph;
-import firm.Program;
 
 public class JavaEasyCompiler {
     /**
@@ -175,30 +178,8 @@ public class JavaEasyCompiler {
      * @return Ok or an according error
      */
     private static Result compileFirm(String filePath, Logger logger, Iterable<Optimization> optimizations) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
-            Lexer lexer = new Lexer(reader);
-            StringTable stringTable = lexer.getStringTable();
-            NamespaceMapper namespaceMapper = new NamespaceMapper();
-            ProgramNode ast = createAttributedAst(reader, logger, lexer, namespaceMapper);
-
-            JFirmSingleton.initializeFirmLinux();
-            logger.info("Initialized libFirm Version: %s.%s",
-                Firm.getMinorVersion(), Firm.getMajorVersion()
-            );
-
-            IRVisitor irv = new IRVisitor(namespaceMapper, stringTable);
-            ast.accept(irv);
-            Lower.lower(irv.getTypeMapper());
-
-            for (Graph graph : Program.getGraphs()) {
-                boolean changed;
-                do {
-                    changed = false;
-                    for (Optimization optimization : optimizations) {
-                        changed |= optimization.optimize(graph);
-                    }
-                } while (changed);
-            }
+        try {
+            createOptimizedIR(filePath, logger, optimizations);
 
             var sourceFile = new File(filePath).getName();
             var assemblyFile = sourceFile + ".s";
@@ -219,6 +200,74 @@ public class JavaEasyCompiler {
                 return Result.GccError;
             }
             
+            return Result.Ok;
+        } catch (CompilerException e) {
+            logger.withName(e.getCompilerStage().orElse(null)).exception(e);
+
+            return e.getResult();
+        } catch (IOException e) {
+            logger.error("unable to read file: %s", e.getMessage());
+
+            return Result.FileInputError;
+        } catch (InterruptedException e) {
+            logger.error("gcc was interrupted: %s", e.getMessage());
+
+            return Result.GccError;
+        }
+    }
+
+    /**
+     * Compiles the file. This includes parsing, semantic analysis, transformation and codegen.
+     *
+     * @param filePath Path of the file (absolute or relative)
+     * @param logger the logger
+     * @return Ok or an according error
+     */
+    private static Result compile(String filePath, Logger logger, Iterable<Optimization> optimizations) {
+        try {
+            createOptimizedIR(filePath, logger, optimizations);
+
+            PatternCollection coll = new PatternCollection();
+            RegisterAllocator allocator = new DumbAllocator();
+
+            List<FunctionInstructions> functions = new ArrayList<>();
+            int blockId = 0;
+            for (Graph graph : Program.getGraphs()) {
+                InstructionSelection selection = InstructionSelection.apply(graph, coll, blockId);
+                Map<Integer, Block> blockMapping = PhiResolver.apply(selection);
+                List<Block> il = ReversePostfixOrder.apply(blockMapping, selection.getBlocks().getStartBlock().getLabel());
+                blockId = selection.getBlocks().newLabel();
+
+                var type = (MethodType) graph.getEntity().getType();
+                int nArgs = type.getNParams();
+                List<String> instructions = allocator.performAllocation(
+                        nArgs, il, selection.getMatcher().getRegisterSizes().toArray(new RegisterSize[0]));
+
+                functions.add(new FunctionInstructions(graph.getEntity().getLdName(), instructions));
+            }
+
+            AssemblyWriter writer = new ElfAssemblyWriter();
+
+            var sourceFile = new File(filePath).getName();
+            var assemblyFile = sourceFile + ".s";
+            var out = new FileOutputStream(assemblyFile);
+            logger.info("assembling program: '%s'", assemblyFile);
+            writer.writeAssembly(functions, out);
+
+            var stdLibrary = System.getenv("STD_LIBRARY_PATH");
+            if (stdLibrary == null || !new File(stdLibrary).exists()) {
+                logger.error("standard library implementation not found");
+                return Result.StandardLibraryError;
+            }
+
+            logger.info("compiling program: 'gcc \"%s\" \"%s\"'", assemblyFile, stdLibrary);
+            var process = Runtime.getRuntime().exec(new String[]{ "gcc", assemblyFile, stdLibrary });
+
+            if (process.waitFor() != 0) {
+                logger.error("gcc failed with exit code %s", process.exitValue());
+                return Result.GccError;
+            }
+
             return Result.Ok;
         } catch (CompilerException e) {
             logger.withName(e.getCompilerStage().orElse(null)).exception(e);
@@ -271,6 +320,41 @@ public class JavaEasyCompiler {
         return ast;
     }
 
+    /**
+     * Parses the file, performs semantic analysis, creates and optimizes the IR.
+     *
+     * @param filePath Path of the file (absolute or relative)
+     * @param logger the logger
+     * @return Ok or an according error
+     */
+    private static void createOptimizedIR(String filePath, Logger logger,
+                                            Iterable<Optimization> optimizations) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+        Lexer lexer = new Lexer(reader);
+        StringTable stringTable = lexer.getStringTable();
+        NamespaceMapper namespaceMapper = new NamespaceMapper();
+        ProgramNode ast = createAttributedAst(reader, logger, lexer, namespaceMapper);
+
+        JFirmSingleton.initializeFirmLinux();
+        logger.info("Initialized libFirm Version: %s.%s",
+                Firm.getMinorVersion(), Firm.getMajorVersion()
+        );
+
+        IRVisitor irv = new IRVisitor(namespaceMapper, stringTable);
+        ast.accept(irv);
+        Lower.lower(irv.getTypeMapper());
+
+        for (Graph graph : Program.getGraphs()) {
+            boolean changed;
+            do {
+                changed = false;
+                for (Optimization optimization : optimizations) {
+                    changed |= optimization.optimize(graph);
+                }
+            } while (changed);
+        }
+    }
+
     public static void main(String[] args) {
         // specify supported command line options
         Options options = new Options();
@@ -283,6 +367,7 @@ public class JavaEasyCompiler {
         runOptions.addOption(new Option("a", "print-ast", true, "try to parse the file contents and output the AST"));
         runOptions.addOption(new Option("c", "check", true, "try to parse the file contents and perform semantic analysis"));
         runOptions.addOption(new Option("f", "compile-firm", true, "transform the file to Firm IR and compile it using the Firm backend"));
+        runOptions.addOption(new Option("co", "compile", true, "compile the file"));
         options.addOptionGroup(runOptions);
 
         var verbosityOptions = new OptionGroup();
@@ -311,6 +396,15 @@ public class JavaEasyCompiler {
         var logger = parseLogger(cmd);
 
         // execute requested function
+        Iterable<Optimization> optimizations;
+        if (cmd.hasOption("0")) {
+            optimizations = Arrays.asList();
+        } else {
+            optimizations = Arrays.asList(
+                    new ConstantOptimization()
+            );
+        }
+
         Result result;
         if (cmd.hasOption("h")) {
             HelpFormatter help = new HelpFormatter();
@@ -344,19 +438,21 @@ public class JavaEasyCompiler {
         } else if (cmd.hasOption("f")) {
             String filePath = cmd.getOptionValue("f");
 
-            Iterable<Optimization> optimizations;
-            if (cmd.hasOption("0")) {
-                optimizations = Arrays.asList();
-            } else {
-                optimizations = Arrays.asList(
-                    new ConstantOptimization()
-                );
-            }
             result = compileFirm(filePath, logger, optimizations);
-        } else {
-            System.err.println("Wrong command line arguments, see --help for supported commands.");
+        } else if (cmd.hasOption("co")) {
+            String filePath = cmd.getOptionValue("co");
 
-            result = Result.CliInputError;
+            result = compile(filePath, logger, optimizations);
+        }  else {
+            if (cmd.getArgs().length == 0) {
+                System.err.println("Wrong command line arguments, see --help for supported commands.");
+
+                result = Result.CliInputError;
+            } else {
+                String filePath = cmd.getArgs()[0];
+
+                result = compile(filePath, logger, optimizations);
+            }
         }
 
         // return exit code from executed function
