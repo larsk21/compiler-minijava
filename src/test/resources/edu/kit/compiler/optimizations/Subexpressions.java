9baf822b87ca046f54e951adfdea7a231f7e0319
Niklas Betten <niklasbetten98@gmail.com>
2022-01-24 14:28:54 +0100

    enables common subexpression elimination

---
+++ src/test/resources/edu/kit/compiler/optimizations/Subexpressions.java
@@ -0,0 +1,25 @@
+class Classic {
+    public int method(int b, int c, int g, int e) {
+
+        int[] arr = new int[10];
+        arr[0] = 2;
+
+        int z = arr[0];
+        int y = arr[0];
+
+        int ab = b/c;
+        int bc = b/c;
+
+        int f = -(-(-(-b)));
+        int k = -(-(-(-b)));
+        int a = b * c + g;
+        int d = b * c * e;
+        return a + d + f + k + z + y + ab + bc;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        return;
+    }
+}
