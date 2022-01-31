9622547c1ce5cccff76e75a3f8ba85da1b6bad2c
Pascal Mehnert <pascalmehnert@posteo.de>
2022-01-31 22:46:39 +0100

    Add some mjtests for loop unrolling

---
+++ mjtest-files/exec/unroller/DeepNesting.java
@@ -0,0 +1,84 @@
+class DeepNesting {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            i = i + 1;
+
+            int j = 0;
+            while (j < 5) {
+                j = j + 1;
+
+                int k = 0;
+                while (k < 5) {
+                    k = k + 1;
+
+                    int l = 0;
+                    while (l < 5) {
+                        l = l + 1;
+
+                        int m = 0;
+                        while (m < 5) {
+                            m = m + 1;
+
+                            int n = 0;
+                            while (n < 5) {
+                                n = n + 1;
+
+                                int q = 0;
+                                while (q < 5) {
+                                    q = q + 1;
+
+                                    int r = 0;
+                                    while (r < 5) {
+                                        r = r + 1;
+
+                                        int s = 0;
+                                        while (s < 5) {
+                                            s = s + 1;
+
+                                            int t = 0;
+                                            while (t < 5) {
+                                                t = t + 1;
+
+                                                int u = 0;
+                                                while (u < 5) {
+                                                    u = u + 1;
+
+                                                    int v = 0;
+                                                    while (v < 5) {
+                                                        v = v + 1;
+
+                                                        int w = 0;
+                                                        while (w < 5) {
+                                                            w = w + 1;
+
+                                                            int x = 0;
+                                                            while (x < 5) {
+                                                                x = x + 1;
+
+                                                                int y = 0;
+                                                                while (y < 5) {
+                                                                    y = y + 1;
+
+                                                                    int z = 0;
+                                                                    while (z < 5) {
+                                                                        z = z + 1;
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(i);
+    }
+}
---
+++ mjtest-files/exec/unroller/SumTo100.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 100) {
+            sum = sum + i;
+            i = i + 1;
+        }
+        System.out.println(sum);
+    }
+}
