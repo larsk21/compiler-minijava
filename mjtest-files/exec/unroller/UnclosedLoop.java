9736fecc179afacf1037f52ce65e07ca3740de62
Pascal Mehnert <pascalmehnert@posteo.de>
2022-02-06 14:43:16 +0100

    Remove unused test

--- mjtest-files/exec/unroller/UnclosedLoop.java
+++ mjtest-files/exec/unroller/UnclosedLoop.java
@@ -1,15 +1,4 @@
 class Test {
-    /*
-    public static void main(String[] args) {
-        while (true) {
-            int j = 0;
-            while (j < 999 && j < 1000) {
-                j = j + 1;
-            }
-        }
-    }
-    */
-
     public static void main(String[] args) {
         int i = 0;
         while (i < 10) {
