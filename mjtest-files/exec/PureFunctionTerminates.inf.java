655dd778dfe010194769d164b9dd9dabc005b2d7
Pascal Mehnert <pascalmehnert@posteo.de>
2022-02-06 14:38:45 +0100

    Ensure pure/const functions terminate

---
+++ mjtest-files/exec/PureFunctionTerminates.inf.java
@@ -0,0 +1,10 @@
+class Test {
+    public static void main(String[] args) {
+        while (true) {
+            int j = 0;
+            while (j < 999 && j < 1000) {
+                j = j + 1;
+            }
+        }
+    }
+}
