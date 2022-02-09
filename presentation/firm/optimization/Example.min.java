33bf39dfd43814e2c18603b6c346d140c817499d
Pascal Mehnert <pascalmehnert@posteo.de>
2022-02-09 19:42:42 +0100

    Update slides based on feedback

---
+++ presentation/firm/optimization/Example.min.java
@@ -0,0 +1,7 @@
+public int foo() {
+    int i = 0;
+    while (i < 4) {
+        i = i + 1;
+    }
+    return i;
+}
---
+++ presentation/firm/transformation/Div.java
@@ -0,0 +1,9 @@
+class Div {
+    public static void main(String[] args) {
+        System.out.println(new Div().foo(42, 15));
+    }
+
+    public int foo(int x, int y) {
+        return x / y;
+    }
+}
